7f3a23f0fdcf4ff2ab612e4f70d977180889a1df
vaish navi <vashnavij950@gmail.com>
2025-01-05 18:28:37 +0530

    M

--- Ad sales prediction-ML.py
+++ Ad sales prediction-ML.py
@@ -1,1 +1,15 @@
-this is a ad sales prediction program using python and scikit learn,numpy, pandas and regression algo was used ,logistic regression was used here
+import pandas as pd
+
+uploaded = 'https://raw.githubusercontent.com/vaishunaviu/vaishu-s/main/DigitalAd_dataset.csv'
+
+ 
+try:
+    dataset = pd.read_csv(uploaded, on_bad_lines='skip')   
+    print("Dataset loaded successfully!\n")
+    print("Dataset Shape:", dataset.shape)  
+    print("\nFirst 5 Rows of the Dataset:")
+    print(dataset.head(5))   
+except Exception as e:
+    print(f"Error loading dataset: {e}")
+x=dataset.iloc[:,:-1].values
+print(x)    
